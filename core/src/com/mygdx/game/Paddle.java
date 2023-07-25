0374ffda9ac8a0cd43a98d447c632c782b430308
Maria1407 <mgimnastika23@gmail.com>
2023-07-25 14:56:50 +0200

    Initial commit

---
+++ core/src/com/mygdx/game/Paddle.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.math.Rectangle;
+
+public class Paddle extends BaseActor
+{
+    public Paddle()
+    {  super();  }
+    
+    public Rectangle getRectangle()
+    {  return new Rectangle( getX(), getY(), getWidth(), getHeight() );  }
+}
